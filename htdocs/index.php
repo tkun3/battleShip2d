5d84bc447f9affd0a068485e7fc3d5345ee3d4cc
tkun3 <tapimlott@live.ca>
2016-11-29 17:29:02 -0800

    base file system and structure

---
+++ htdocs/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="battleship.css">
+	<title>Super Ships 2D</title>
+</head>
+<form action="/index.php" method="POST" name="position" id="position">
+				<input type="hidden" name="action" id="action">
+				<input type="hidden" name="sid" id="sid" value="2">
+</form>
+
+</html>
