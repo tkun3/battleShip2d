1de88617a46f7b8a8822df13ba5559203e918616
tkun3 <tapimlott@live.ca>
2016-11-30 00:51:51 -0800

    addded stuff

--- htdocs/index.php
+++ htdocs/index.php
@@ -12,8 +12,30 @@
 				<div class="title" align="center">
 					<h1>SUPER BATTLE SHIPS 2D</h1>
 				</div>
+
+				<?php
+
+				$ship1;
+
+				$shipCount = 1;
+
+				for($i=1; $i<9; $i++){
+					for($j=1; $j<9; $j++){
+						if(isset($_POST['checkBox15']) && $_POST['checkBox15'] == 'Yes'){
+							$ship1 = 'checkBox'.'$i$j';
+							$shipCount++;
+						}
+					}
+				}
+
+				if(isset($ship1)){
+					echo asdfasdfdsfasfdsfasfasdfasdfasdfasdfa;
+				}
+				 ?>
+
 				<div class="leftScreen" align="center">
-					<form action="/index.php" method="POST">
+						<!-- Dynamic Ship Selection Table -->	<!-- Dynamic Ship Selection Table -->	<!-- Dynamic Ship Selection Table -->
+					<form action="index.php" method="POST">
 					<table id="player1Table" class="tablePosition">
 						<tbody>
 
@@ -24,25 +46,40 @@
 								j = j+i-1;
 								x[j].style.backgroundColor = "#9999ff";
 						}
-
 						</script>
+
 						<?php
 						for($i=1; $i<9; $i++){
 							echo "<tr name='row$i'> \n";
 							for($j=1; $j<9; $j++){
-								echo "<td id='col$i$j' onClick='btnClick($i,$j);'  name='col$i$j'> <input type='checkbox' class='cell' name='button$i$j'> </td> \n";
+								echo "<td id='col$i$j' onClick='btnClick($i,$j);'  name='col$i$j'> <input type='checkbox' class='cell' value='Yes' name='checkBox$i$j'> </td> \n";
 					 		}
 						}
 						?>
 						</tr>
 					</tbody>
 					</table>
+					<input type="submit" value="Submit">
 					</form>
+					<!-- Dynamic Ship Selection Table -->	<!-- Dynamic Ship Selection Table -->	<!-- Dynamic Ship Selection Table -->
 				</div>
 				<div class="leftScore" align="center">
 					<h2> PLAYER 1 </h2>
 				</div>
 				<div class="rightScreen" align="center">
+					<table id="player2Table" class="tablePosition">
+						<tbody>
+						<?php
+						for($i=1; $i<9; $i++){
+							echo "<tr name='row2$i'> \n";
+							for($j=1; $j<9; $j++){
+								echo "<td id='col2$i$j' name='col2$i$j'> <input type='checkbox' class='cell' name='button2$i$j'> </td> \n";
+					 		}
+						}
+						?>
+						</tr>
+					</tbody>
+					</table>
 				</div>
 				<div class="rightScore" align="center">
 					<h2> PLAYER 2 </h2>
